7265cfaf6721b3c6e62b24f4c197f97e4fe0e95d
owen-eternal <olwethuphakade@gmail.com>
2021-08-09 17:36:31 +0200

    fixed enviromnent variables for database conn

--- aura-backend/src/db.js
+++ aura-backend/src/db.js
@@ -1,12 +1,13 @@
 const Pool = require('pg').Pool;
 
 // establish a database connection
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'aura',
-    password: 'postgres',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
   })
     
 module.exports = pool
