e121f11da0fb9dbe69d428fe5dbd9c19ee5ffb8c
owen-eternal <olwethuphakade@gmail.com>
2021-08-10 13:19:53 +0200

    installed cors

--- aura-backend/src/init.js
+++ aura-backend/src/init.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const Express = require('express')
 const auth = require('./auth/authRoute')
+const cors = require('cors')
 
 //creating an instance of our application
 //application factory pattern
@@ -9,6 +10,7 @@ function create_app(){
     const app = Express()
 
     //inititiate application level middleware.
+    app.use(cors())
     app.use(Express.json())
 
 
