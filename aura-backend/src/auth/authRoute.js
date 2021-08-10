9e2fd70be1b7c8beedc8372ecb063b81f216453d
owen-eternal <olwethuphakade@gmail.com>
2021-08-10 10:47:20 +0200

    codebade for login endpoint and comments

--- aura-backend/src/auth/authRoute.js
+++ aura-backend/src/auth/authRoute.js
@@ -40,30 +40,39 @@ auth.post('/register', async (request, response) => {
 // Route for handling log-ins.
 auth.post('/login', async (req, res) => {
 
+    // retrieve data from request body
     const { email, password } = req.body
 
     try{
 
         // define the sql query.
         const queryString = 'SELECT id, username, email, passwrd FROM service_user WHERE email = $1'
-
+        
+        // receive Object from backend
         const queryObject = await pool.query(queryString, [email])
 
+        // define a user object
         const user = queryObject.rows[0]
 
+        // compare client password vs database password
         const isMatch = await bcrypt.compare(password, user.passwrd)
 
+        // check if theres a match or not
         if (!isMatch) return res.status(403).send('incorrect credentials')
 
+        // define payload object
         const payload = {
             id : user.id,
             username : user.username
         }
         
+        // create untimed token
         const token = await JWT.sign(payload, process.env.SECRET_KEY)
 
+        // send token to frontend
         return res.status(200).json({access_token : token})
         
+    // throw error
     }catch(error){
         res.status(403).send('incorrect credentials')
         console.error(error)
