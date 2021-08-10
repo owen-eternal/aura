6bc726f5d1560d1cb558787f01181649a15ff04f
owen-eternal <olwethuphakade@gmail.com>
2021-08-10 09:40:36 +0200

    change port variable name

--- aura-backend/server.js
+++ aura-backend/server.js
@@ -3,7 +3,7 @@ const create_app = require('./src/init.js')
 
 app = create_app()
 
-const PORT = process.env.PORT
+const PORT = process.env.SERVER_PORT
 
 app.listen(PORT, ()=>{
     console.log(`listening at port: http://localhost:${PORT}/`)
