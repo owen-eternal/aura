8d5d0980fbc4513ce7e3c76057bdd797bb0d7c58
owen-eternal <olwethuphakade@gmail.com>
2021-08-11 12:30:52 +0200

    codebase for creating an alert

--- aura-backend/src/panic/panicRoute.js
+++ aura-backend/src/panic/panicRoute.js
@@ -1,17 +1,41 @@
-const { request, response } = require('express');
-
 const alert = require('express').Router();
+const { loadUser } = require('../middleware/verifications');
+const { loginRequired, userRole } = require('../middleware/permissions');
+const pool = require('../db');
 
-alert.post('/create', (request, response) =>{
-    // must be authenticated and authorised  
-    // to created an alert.
-    response.status(200).json({payload: request.body})
+
+alert.use(loadUser, loginRequired)
+
+//user role
+alert.post('/create-alert', userRole('basic'), async (request, response) =>{
+
+    try{
+
+        const userId = parseInt(request.user.rows[0].id)
+
+        const { geolocation, status } = request.body
+
+        const query = 'INSERT INTO service_alert (geolocation, stat, user_id) VALUES ($1, $2, $3) RETURNING id, user_id'
+
+        const alert = await pool.query(query, [geolocation, status, userId])
+
+        return response.status(201).json(
+            {
+                message : "alert lodged on your behalf",
+                alert_details: alert.rows[0]
+        })
+
+    }catch(error){console.log(error)}
 })
 
-alert.delete('/cancel/:id', (request, response) =>{
+alert.delete('/cancel-alert/:id', userRole('basic'), (request, response) =>{
     // must be authenticated and authorised  
     // to created an alert.
-    response.status(201).send(`Alert ${request.params.id} canceled`)
+})
+
+alert.get('/fetch-all-alerts', userRole('admin'), (request, response) =>{
+    // must be authenticated and authorised  
+    // to created an alert.
 })
 
 module.exports = alert
