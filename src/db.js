7624a450525fbd72b73379806b81bc065f26aec6
owen-eternal <olwethuphakade@gmail.com>
2021-08-13 14:18:41 +0200

    checking database connection

--- src/db.js
+++ src/db.js
@@ -5,11 +5,7 @@ const connectionString = process.env.DATABASE_URL
 
 // establish a database connection
 const pool = new Pool({
-    user: process.env.USER, 
-    host: process.env.HOST, 
-    database: process.env.DATABASE, 
-    password: process.env.PASSWORD, 
-    port: process.env.PORT
+    connectionString,
 })
-    
+console.log(connectionString)    
 module.exports = pool
