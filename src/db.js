9193d6551417b9602031421cb24fe865d4c7a5e5
owen-eternal <olwethuphakade@gmail.com>
2021-08-13 14:00:16 +0200

    checking database connection

--- src/db.js
+++ src/db.js
@@ -1,7 +1,11 @@
 const Pool = require('pg').Pool;
 const config = require('./config')
 
+const connectionString = process.env.DATABASE_URL
+
 // establish a database connection
-const pool = new Pool(config.db.connectionString)
+const pool = new Pool({
+    connectionString,
+})
     
 module.exports = pool
